7aacba45a6de314782e74591f7bc907cb14e96af
AngelDv18 <133921931+AngelDv18@users.noreply.github.com>
2023-10-09 15:32:11 -0500

    Agregar archivos de proyecto.

---
+++ 24CV-WEB/Controllers/EjemplosController1.cs
@@ -0,0 +1,24 @@
+﻿using _24CV_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _24CV_WEB.Controllers
+{
+    public class EjemplosController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Angel";
+            persona.Apellidos = "Garcia";
+            persona.FechaNacimiento = new DateTime(2004, 11, 24);
+
+
+            return View(persona);
+        }
+    }
+}
